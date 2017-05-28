d0ed71208d754fd6e5e8c136652991be2419f248
Kamil21180 <kamil.przybycien@gmial.com>
2017-05-28 22:36:29 +0200

    add SummaryFormActivity

--- app/src/main/java/uek/krakow/pl/androidinvoicegenerator/GoodsFormActivity.java
+++ app/src/main/java/uek/krakow/pl/androidinvoicegenerator/GoodsFormActivity.java
@@ -2,6 +2,10 @@ package uek.krakow.pl.androidinvoicegenerator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class GoodsFormActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class GoodsFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goods_form);
+
+        Spinner spinner = (Spinner) findViewById(R.id.spinner_stawkaVAT);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(GoodsFormActivity.this, parent.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
+
+
 }
